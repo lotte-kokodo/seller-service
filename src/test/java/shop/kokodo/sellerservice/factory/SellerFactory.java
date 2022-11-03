d46076f00d985585fa480bfcaeb0d5755caf0306
hellonayeon <hellonykwon@gmail.com>
2022-11-03 11:06:32 +0900

    Hotfix: Seller 객체 생성 코드 수정 #KOK-88

--- src/test/java/shop/kokodo/sellerservice/factory/SellerFactory.java
+++ src/test/java/shop/kokodo/sellerservice/factory/SellerFactory.java
@@ -17,7 +17,7 @@ import shop.kokodo.sellerservice.entity.Seller;
 public class SellerFactory {
 
     public static Seller createSeller(Long id){
-        return new Seller(id, "namhyeop1", "!namhyeop1234", "010-3333-3333",
+        return new Seller(id, null, "namhyeop1", "!namhyeop1234", "010-3333-3333",
                 "namhyeop@gmail.com", true, true, "1996-12-12",
                 "KIMNAMHYEOP", 100000L, "서울특별시 강남구", "서울특별시 강동구",
                 "롯데택배", "롯데택배", true, true,
